01a38c767eb35c9624c3fe16ad3953485292f0ae
Luke Vella <luke.vella.0697@gmail.com>
2025-10-18 16:28:15 +0200

    FirstCommit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Nunito } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -10,6 +11,12 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const nunito = Nunito({
+  variable: "--font-nunito",
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
@@ -23,11 +30,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${nunito.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,92 @@
+"use client";
+
 import Image from "next/image";
+import { ThemeToggle } from "@/components/theme-toggle";
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  return (
+    <div className="font-nunito h-screen flex justify-center">
+      <div className="w-full grid grid-rows-[60px_1fr_40px] gap-4 px-6 lg:px-">
+        <header className="g-header flex align-center gap-4 px-4 items-center">
+          <div className="g-header-left text-3xl">Luke Vella</div>
+          <div className="g-header-middle flex-1">
+            <Button variant="link" size="lg" className="ml-4">
+              Portfolio
+            </Button>
+            <Button variant="link" size="lg" className="ml-4">
+              Some side projects
+            </Button>
+            <Button variant="link" size="lg" className="ml-4">
+              Dog pics.
+            </Button>
+          </div>
+
+          <div className="g-header-right">
+            <ThemeToggle />
+          </div>
+        </header>
+
+        <main className="g-body flex flex-col gap-6 items-center px-4 py-30 overflow-y-auto min-h-0">
+          <div className="flex flex-col gap-20">
+            <div className="flex flex-col items-center gap-10">
+              <div className="flex flex-col">
+                <h1 className="text-8xl">LUKE VELLA</h1>
+
+                <div className="flex text-sm  gap-2 lg:gap-4 flex-wrap justify-start">
+                  <div className="whitespace-nowrap">
+                    Senior front-end Developer
+                  </div>
+                  <div>|</div>
+                  <div className="whitespace-nowrap">Game Dev Hobbyist</div>
+                  <div>|</div>
+                  <div className="whitespace-nowrap">Coffee Addict</div>
+                </div>
+              </div>
+            </div>
+
+            <div className="g-row">
+              <p className="text-2xl text-muted-foreground self-center text-center py-4">
+                Welcome to my world.
+              </p>
+            </div>
+
+            <div className="g-row flex flex-col gap-8">
+              <h2 className="text-2xl">{`Components, fresh out the oven.`}</h2>
+
+              <div className="g-row items-start flex flex-col gap-4 border-b pb-8">
+                <h3 className="text-xl">{`Button Variants`}</h3>
+                <div className="flex flex-wrap gap-4">
+                  <Button variant="default">Default</Button>
+                  <Button variant="destructive">Destructive</Button>
+                  <Button variant="outline">Outline</Button>
+                  <Button variant="secondary">Secondary</Button>
+                  <Button variant="ghost">Ghost</Button>
+                  <Button variant="link">Link</Button>
+                </div>
+              </div>
+
+              <div className="g-row items-start flex flex-col gap-4 border-b pb-8">
+                <h3 className="text-xl">{`Calendar`}</h3>
+                <div className="flex self-center flex-wrap gap-4">
+                  <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                    className="rounded-lg border"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+        <div className="g-footer">test</div>
+      </div>
     </div>
   );
 }
